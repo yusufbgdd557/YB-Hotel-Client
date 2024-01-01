0e3b6d0c8ec29850cc99f588d5173765f02ae380
yusufbgdd557 <yusufbgdd557@gmail.com>
2024-01-01 10:21:17 +0300

    Initialize frontend

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import React from "react"
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
+import './App.css'
+import AddRoom from "./components/room/AddRoom"
+import ExistingRooms from "./components/room/ExistingRooms"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from "./components/home/Home"
+import EditRoom from "./components/room/EditRoom"
+
+function App() {
+  return (
+    <>
+      <main>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/add-room" element={<AddRoom />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRooms />} />
+          </Routes>
+        </Router>
+      </main>
+    </>
+  )
+}
+
+export default App
