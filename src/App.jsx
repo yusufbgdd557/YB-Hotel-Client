fa448f72c5d0a102b718d3420685baed2d16ed92
yusufbgdd557 <yusufbgdd557@gmail.com>
2024-01-03 02:03:52 +0300

    Added some components
    Added many components to client side

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,31 @@ import ExistingRooms from "./components/room/ExistingRooms"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 import Home from "./components/home/Home"
 import EditRoom from "./components/room/EditRoom"
+import NavBar from "./components/layout/Navbar"
+import Footer from "./components/layout/Footer"
+import RoomListing from "./components/room/RoomListing"
+import Admin from "./components/admin/Admin"
+import BookingSuccess from "./components/bookings/BookingSuccess"
+import Checkout from "./components/bookings/Checkout"
 
 function App() {
   return (
     <>
       <main>
         <Router>
+          <NavBar />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/add-room" element={<AddRoom />} />
+            <Route path="/browse-all-rooms" element={<RoomListing />} />
             <Route path="/edit-room/:roomId" element={<EditRoom />} />
             <Route path="/existing-rooms" element={<ExistingRooms />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/book-room/:roomId" element={<Checkout />} />
+            <Route path="/booking-success" element={<BookingSuccess />} />
           </Routes>
         </Router>
+        <Footer />
       </main>
     </>
   )
--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import MainHeader from "../layout/MainHeader"
+import HotelService from "../common/HotelService"
+import Parallax from "../common/Parallax"
+import RoomCarousel from "../common/RoomCarousel"
+
 
 const Home = () => {
-  return (
-    <div>
-        <h2>Home Room</h2>
-    </div>
-  )
+	
+	return (
+		<section>
+			<MainHeader />
+			<div className="container"> 
+        <RoomCarousel />
+				<Parallax />    
+				<HotelService />
+				<Parallax />    
+			</div>
+		</section>
+	)
 }
 
 export default Home
